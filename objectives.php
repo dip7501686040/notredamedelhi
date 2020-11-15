c2fcea344c082897da7eb550ddb276592fa0cead
Neelakshi_T <t.neelakshi935@gmail.com>
2020-11-15 19:35:16 +0530

    neelu changes

--- objectives.php
+++ objectives.php
@@ -1,97 +1,91 @@
-<html>
 <?php
 require_once('header.php');
 ?>
-
-<head>
-<title>Aims and objectives</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
-.main2{
-		display:flex;
-		margin:10px;
-		padding:10px;
-		background-color:#d5e3dd;
-		background:linear-gradient(to left,#dcf1f5,white);
+	.main2 
+	{
+		display: flex;
+		margin: 10px;
+		padding: 10px;
+		background-color: #d5e3dd;
+		background: linear-gradient(to left, #dcf1f5, white);
 	}
-	
-	.four{
-		
-		width:33%;
-		height:97%;
-		text-align:justify;
-		font-size:20px;
+
+	.four {
+
+		width: 50%;
+		height: 97%;
+		text-align: justify;
+		font-size: 20px;
 		margin-right: 18px;
 	}
-	.five{
-		width:33%;
-		height:97%;
-		text-align:justify;
-		font-size:18px;
-		margin-right:20px;	
+
+	
+	@media (max-width: 768px) {
+		.main2 {
+			display: block;
+			width: 100%;
+			height: auto;
+			margin: 0px;
+			padding: 0px;
+		}
 	}
-	
-	@media (max-width: 768px){
-		.main2{
-			display:block;
-			width:100%;
-			height:auto;
-			margin:0px;
-			padding:0px;
-	
-	
 </style>
-</head>
+
 <body>
+<div class="container">
 <div class="main2">
-<div class="four">
-<h3>FAITH IN GOD</h3>
+		<div class="four">
+			<h3>FAITH IN GOD</h3>
 
-(a) A deep inter-relationship with God that permeates his/her daily life and his/her contact with others.<br>
-(b) Personal and group prayer to God whom he/she considers to be his/ her loving Father.<br>
-(c) Acceptance of and respect for all religions.<br>
-(d) Deepening and broadening of his/her religious convictions.<br>
+			(a) A deep inter-relationship with God that permeates his/her daily life and his/her contact with others.<br>
+			(b) Personal and group prayer to God whom he/she considers to be his/ her loving Father.<br>
+			(c) Acceptance of and respect for all religions.<br>
+			(d) Deepening and broadening of his/her religious convictions.<br>
 
-<h3>CONSCIENTISATION TOWARDS JUSTICE AND EQUALITY</h3>
+			<h3>CONSCIENTISATION TOWARDS JUSTICE AND EQUALITY</h3>
 
-Every Notre Dame student must be imbued with the fact that she/he is living in a very unjust society 
-and that she/he must do all within her/ his power to bring about a radical change in the present structures.
- Therefore, she/he must develop:<br>
-(a) A genuine love for the poor and the needy. She/he must programme regular sessions during the week in which she/he
- has some positive contact and communications with the poor, the illiterate, and the needy.<br>
-(b) A loving concern for the different types of oppressions suffered by the women, the minorities, the under
- paid labourers, and the weaker sections.<br>
-(c) Outstanding speaking and writing ability so that she/he can use her/ his talents as a platform to speak
- and write against any injustice and oppression suffered by others.<br>
-(d) A willingness to involve himself/herself in programmes, projects, camps, workshops, etc. in which she/he will 
-be actively involved in helping the financially poor or deprived sections.
+			Every Notre Dame student must be imbued with the fact that she/he is living in a very unjust society
+			and that she/he must do all within her/ his power to bring about a radical change in the present structures.
+			Therefore, she/he must develop:<br>
+			(a) A genuine love for the poor and the needy. She/he must programme regular sessions during the week in which she/he
+			has some positive contact and communications with the poor, the illiterate, and the needy.<br>
+			(b) A loving concern for the different types of oppressions suffered by the women, the minorities, the under
+			paid labourers, and the weaker sections.<br>
+			(c) Outstanding speaking and writing ability so that she/he can use her/ his talents as a platform to speak
+			and write against any injustice and oppression suffered by others.<br>
+			(d) A willingness to involve himself/herself in programmes, projects, camps, workshops, etc. in which she/he will
+			be actively involved in helping the financially poor or deprived sections.
+		</div>
+		<div class="four">
+			<h3>SENSE OF COMMUNITY</h3>
+
+			(a) Realising that he/she belongs to a country that is rich in cultural heritage, she/he develops patriotism and pride for India.<br>
+			(b) As part of the school family, she/he especially develops awareness of the needs of others and responds to their unspoken wishes/
+			demands/ requests by giving generously of her/his time, talents, and service.<br>
+			(c) She/he inculcates in herself/himself those social virtues which are a genuine response to others: responsibility, courtesy,
+			honesty, punctuality, loyalty, obedience, womanliness/manliness, generosity, and diligence.<br>
+			(d) She/he integrates herself/himself readily with others of various social, religious, cultural and national backgrounds.<br>
+			(e) As a citizen of the world, she/he takes keen interest in what is happening in other countries and easily discusses and
+			empathises with the social and economic situations prevalent in them.<br>
+
+			<h3>ACADEMIC VALUES</h3>
+
+			(a) Acquisition of knowledge and sharpening of her/his memory, speed and accuracy.<br>
+			(b) Acquisition of the following skills: observing, recording, quoting, illustrating, demonstrating, analysing,
+			outlining, summarising, integrating, evaluating and criticising.<br>
+			(c) Power of expression through public speaking: debates, dramatics, elocution, extemporaneous speeches, dance, music and creative writing.<br>
+			(d) An on-going quest for knowledge that will last throughout her/his life.<br>
+			(e) An inner discernment which guides her/him to choose the correct values even when there is a dichotomy between the
+			home and the school training or between society and the school.<br>
+			(f) Physical exercises, sports and athletic skills which help her/him develop a sound mind in a sound body.<br>
+
+			
+		</div>
+	</div>
 </div>
-<div class="five">
-<h3>SENSE OF COMMUNITY</h3>
 
-(a) Realising that he/she belongs to a country that is rich in cultural heritage, she/he develops patriotism and pride for India.<br>
-(b) As part of the school family, she/he especially develops awareness of the needs of others and responds to their unspoken wishes/
-demands/ requests by giving generously of her/his time, talents, and service.<br>
-(c) She/he inculcates in herself/himself those social virtues which are a genuine response to others: responsibility, courtesy,
- honesty, punctuality, loyalty, obedience, womanliness/manliness, generosity, and diligence.<br>
-(d) She/he integrates herself/himself readily with others of various social, religious, cultural and national backgrounds.<br>
-(e) As a citizen of the world, she/he takes keen interest in what is happening in other countries and easily discusses and
- empathises with the social and economic situations prevalent in them.<br>
- 
- <h3>ACADEMIC VALUES</h3>
-
-(a) Acquisition of knowledge and sharpening of her/his memory, speed and accuracy.<br>
-(b) Acquisition of the following skills: observing, recording, quoting, illustrating, demonstrating, analysing,
- outlining, summarising, integrating, evaluating and criticising.<br>
-(c) Power of expression through public speaking: debates, dramatics, elocution, extemporaneous speeches, dance, music and creative writing.<br>
-(d) An on-going quest for knowledge that will last throughout her/his life.<br>
-(e) An inner discernment which guides her/him to choose the correct values even when there is a dichotomy between the
- home and the school training or between society and the school.<br>
-(f) Physical exercises, sports and athletic skills which help her/him develop a sound mind in a sound body.<br>
-</div>
-</div>
-
-	
-<?php
-require_once('footer.php');
-?>
+</body>
+	<?php
+	require_once('footer.php');
+	?>
